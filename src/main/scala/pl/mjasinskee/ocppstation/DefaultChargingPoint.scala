e1fa481d43913f1244312890ccdfc0525db700a9
marcin jasinski <m.jasinskee@gmail.com>
2019-03-18 20:40:27 +0100

    charger is sending boot notification on start

--- src/main/scala/pl/mjasinskee/ocppstation/DefaultChargingPoint.scala
+++ src/main/scala/pl/mjasinskee/ocppstation/DefaultChargingPoint.scala
@@ -1,5 +1,46 @@
 package pl.mjasinskee.ocppstation
 
-object DefaultChargignPoint {
+import com.thenewmotion.ocpp.messages.v1x.{CancelReservationReq, CancelReservationRes, ChangeAvailabilityReq, ChangeAvailabilityRes, ChangeConfigurationReq, ChangeConfigurationRes, ChargePoint, ChargePointDataTransferReq, ChargePointDataTransferRes, ClearCacheRes, ClearChargingProfileReq, ClearChargingProfileRes, GetCompositeScheduleReq, GetCompositeScheduleRes, GetConfigurationReq, GetConfigurationRes, GetDiagnosticsReq, GetDiagnosticsRes, GetLocalListVersionRes, RemoteStartTransactionReq, RemoteStartTransactionRes, RemoteStopTransactionReq, RemoteStopTransactionRes, ReserveNowReq, ReserveNowRes, ResetReq, ResetRes, SendLocalListReq, SendLocalListRes, SetChargingProfileReq, SetChargingProfileRes, TriggerMessageReq, TriggerMessageRes, UnlockConnectorReq, UnlockConnectorRes, UpdateFirmwareReq}
 
+import scala.concurrent.Future
+
+class DefaultChargingPoint extends ChargePoint {
+
+  override def remoteStartTransaction(req: RemoteStartTransactionReq): Future[RemoteStartTransactionRes] = Future.successful(RemoteStartTransactionRes(accepted = true))
+
+  override def remoteStopTransaction(req: RemoteStopTransactionReq): Future[RemoteStopTransactionRes] = ???
+
+  override def unlockConnector(req: UnlockConnectorReq): Future[UnlockConnectorRes] = ???
+
+  override def getDiagnostics(req: GetDiagnosticsReq): Future[GetDiagnosticsRes] = ???
+
+  override def changeConfiguration(req: ChangeConfigurationReq): Future[ChangeConfigurationRes] = ???
+
+  override def getConfiguration(req: GetConfigurationReq): Future[GetConfigurationRes] = ???
+
+  override def changeAvailability(req: ChangeAvailabilityReq): Future[ChangeAvailabilityRes] = ???
+
+  override def clearCache: Future[ClearCacheRes] = ???
+
+  override def reset(req: ResetReq): Future[ResetRes] = ???
+
+  override def updateFirmware(req: UpdateFirmwareReq): Future[Unit] = ???
+
+  override def sendLocalList(req: SendLocalListReq): Future[SendLocalListRes] = ???
+
+  override def getLocalListVersion: Future[GetLocalListVersionRes] = ???
+
+  override def dataTransfer(req: ChargePointDataTransferReq): Future[ChargePointDataTransferRes] = ???
+
+  override def reserveNow(req: ReserveNowReq): Future[ReserveNowRes] = ???
+
+  override def cancelReservation(req: CancelReservationReq): Future[CancelReservationRes] = ???
+
+  override def clearChargingProfile(req: ClearChargingProfileReq): Future[ClearChargingProfileRes] = ???
+
+  override def getCompositeSchedule(req: GetCompositeScheduleReq): Future[GetCompositeScheduleRes] = ???
+
+  override def setChargingProfile(req: SetChargingProfileReq): Future[SetChargingProfileRes] = ???
+
+  override def triggerMessage(req: TriggerMessageReq): Future[TriggerMessageRes] = ???
 }
--- src/main/scala/pl/mjasinskee/ocppstation/StationApp.scala
+++ src/main/scala/pl/mjasinskee/ocppstation/StationApp.scala
@@ -1,5 +1,47 @@
 package pl.mjasinskee.ocppstation
 
-class StationApp {
+import java.net.URI
+
+import com.thenewmotion.ocpp.json.api.client.OcppJsonClient
+import com.thenewmotion.ocpp.messages.v1x.BootNotificationReq
+import com.thenewmotion.ocpp.{Version, Version1X}
+
+import scala.concurrent.{ExecutionContext}
+
+import ExecutionContext.Implicits.global
+
+object StationApp extends App {
+
+  private val chargerId = args.headOption.getOrElse("NUON_0001")
+  private val centralSystemUri = if (args.length >= 2) args(1) else "ws://localhost:8080/ocppws"
+  private val versions =
+    if (args.length >= 3)
+      args(2).split(",").map { version =>
+        Version.withName(version).getOrElse(sys.error(s"Unrecognized version(s): ${args(2)}"))
+      }.toSeq.collect({ case v: Version1X => v })
+    else
+      Seq(Version.V16)
+
+  val ocppJsonClient = OcppJsonClient.forVersion1x(chargerId, new URI(centralSystemUri), versions) {
+    new DefaultChargingPoint
+  }
+
+  ocppJsonClient.onClose.foreach(_ => println("OCPP connection closed"))
+
+  println(s"Connected using OCPP version ${ocppJsonClient.ocppVersion}")
+
+  for {_ <- ocppJsonClient.send(BootNotificationReq(
+    chargePointVendor = "The New Motion",
+    chargePointModel = "Lolo 47.6",
+    chargePointSerialNumber = Some("123456"),
+    chargeBoxSerialNumber = None,
+    firmwareVersion = None,
+    iccid = None,
+    imsi = None,
+    meterType = None,
+    meterSerialNumber = None)).map { res =>
+    System.out.println(res.status)
+  }
+  } yield ()
 
 }
